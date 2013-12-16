57566e3111b1e3dbde710abdc8d9ce57bc69a6c0
conroy.charlie <conroy.charlie@87205bb6-ae1b-52ed-3b2a-8dbdabc6bc5c>
2013-12-16 19:15:44 +0000

    added write_a_model routine

--- src/sfvars.f90
+++ src/sfvars.f90
@@ -20,8 +20,8 @@ MODULE SFVARS
   !apply template error function? (only works for SDSS stacks)
   INTEGER, PARAMETER :: apply_temperrfcn=0
   !fit only a subset of the full model parameters 
-  !  e.g., no IMF, no nuisance parameters, no "exotic" elements
-  INTEGER, PARAMETER :: fitsimple=0
+  !e.g., no IMF, no nuisance parameters, no "exotic" elements
+  INTEGER, PARAMETER :: fitsimple=1
   !force [Na/Fe]=[Mg/Fe]
   INTEGER, PARAMETER :: force_nafe=0
   !force the IMF to be a MW IMF if set
@@ -116,9 +116,9 @@ MODULE SFVARS
      REAL(DP) :: age=13.5,feh=0.0,afe=0.0,nhe=0.0,cfe=0.0,nfe=0.0,nafe=0.0,&
           mgfe=0.0,sife=0.0,kfe=0.0,cafe=0.0,tife=0.0,vfe=0.0,crfe=0.0,&
           mnfe=0.0,cofe=0.0,nife=0.0,cufe=0.0,rbfe=0.0,srfe=0.0,yfe=0.0,zrfe=0.0,&
-          bafe=0.0,eufe=0.0,teff=0.0,imf1=1.3,imf2=2.3,logfy=1E-5,sigma=0.0,&
-          sigma2=0.0,velz=0.0,velz2=0.0,logm7g=1E-5,hotteff=20.0,loghot=1E-5
-     REAL(DP), DIMENSION(neml) :: logemnorm=0.0
+          bafe=0.0,eufe=0.0,teff=0.0,imf1=1.3,imf2=2.3,logfy=-5.0,sigma=0.0,&
+          sigma2=0.0,velz=0.0,velz2=0.0,logm7g=-5.0,hotteff=20.0,loghot=-5.0
+     REAL(DP), DIMENSION(neml) :: logemnorm=-5.0
      REAL(DP) :: chi2=huge_number
   END TYPE PARAMS
   
