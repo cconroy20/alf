df8ddf7c0d720477acb350b82c86897ecd41eabf
conroy.charlie <conroy.charlie@87205bb6-ae1b-52ed-3b2a-8dbdabc6bc5c>
2014-01-22 03:50:38 +0000

    add_response

---
+++ src/add_response.f90
@@ -0,0 +1,39 @@
+SUBROUTINE ADD_RESPONSE(spec,pos,range,dr,vr,solar,plus,minus)
+
+  USE sfvars
+  IMPLICIT NONE
+
+  INTEGER, INTENT(in)  :: vr
+  REAL(SP), INTENT(in) :: range
+  REAL(DP), INTENT(in) :: pos,dr
+  REAL(DP), DIMENSION(nl), INTENT(inout) :: spec
+  REAL(DP), DIMENSION(nl) :: tmp,tmpr
+  REAL(DP), DIMENSION(nage_rfcn,nl), INTENT(in) :: plus,solar
+  REAL(DP), DIMENSION(nage_rfcn,nl), INTENT(in), OPTIONAL :: minus
+
+  !---------------------------------------------------------------!
+  !---------------------------------------------------------------!
+ 
+  IF (PRESENT(minus)) THEN 
+
+ 
+     IF (pos.GT.0.0) THEN
+        tmpr = dr*plus(vr+1,:)/solar(vr+1,:) + (1-dr)*plus(vr,:)/solar(vr,:)
+        tmp  = 1 + (tmpr-1)*pos/range
+        spec = spec * tmp
+     ELSE IF (pos.LT.0.0) THEN     
+        tmpr = dr*minus(vr+1,:)/solar(vr+1,:) + (1-dr)*minus(vr,:)/solar(vr,:)
+        tmp  = 1 + (tmpr-1)*ABS(pos)/range
+        spec = spec * tmp
+     ENDIF
+
+  ELSE
+ 
+     tmpr = dr*plus(vr+1,:)/solar(vr+1,:) + (1-dr)*plus(vr,:)/solar(vr,:)
+     tmp  = 1 + (tmpr-1)*pos/range
+     spec = spec * tmp
+ 
+  ENDIF
+
+
+END SUBROUTINE ADD_RESPONSE
