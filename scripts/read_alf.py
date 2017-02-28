8c54ab88aff2e04bbbb88b85ca4b03f0b3278162
Alexa Villaume <avillaum@ucsc.edu>
2017-02-28 12:26:36 -0800

    Working on keeping this up-to-date.

--- scripts/read_alf.py
+++ scripts/read_alf.py
@@ -12,9 +12,8 @@ from matplotlib.backends.backend_pdf import PdfPages
 from astropy.io import ascii
 
 class Alf(object):
-    def __init__(self, path, fname, legend):
-        self.name = fname
-        self.path = '{0}/{1}'.format(path, fname)
+    def __init__(self, path, legend):
+        self.path = path
         self.legend = legend
         try:
             self.indata = np.loadtxt('{0}.dat'.format(self.path))
@@ -39,9 +38,15 @@ class Alf(object):
                   'loghot','fy_logage','logtrans', 'logemline_H',
                   'logemline_Oiii','logemline_Sii', 'logemline_Ni',
                   'logemline_Nii','jitter','IMF3', 'logsky', 'IMF4',
-                  'ML_r','ML_i','ML_k','MW_r', 'MW_i','MW_k']
+
+                  #'ML_r','ML_i','ML_k','MW_r', 'MW_i','MW_k']
+                  'h3', 'h4', 'ML_r','ML_i','ML_k','MW_r', 'MW_i','MW_k']
 
 
+
+        #results = ascii.read('{0}.sum'.format(self.path))
+        #print len(results.colnames), len(self.labels)
+        #sys.exit()
         results = ascii.read('{0}.sum'.format(self.path), names=self.labels)
 
         if len(self.labels) != len(results.colnames):
@@ -71,17 +76,17 @@ class Alf(object):
         Check the values of the nuisance parameters
         and raise a warning if they are too large.
         """
-        warning = ('\n For {0} {1}={2}, which is '
-                   'larger than acceptable. \n')
-        if self.params['logm7g'] > -1.0:
-            warnings.warn(warning.format(self.name, 'logm7g',
-                          self.params['logm7g']))
-        elif self.params['Teff'] > -1.0:
-            warnings.warn(warning.format(self.name, 'Teff',
-                          self.params['Teff']))
-        elif self.params['loghot'] > -1.0:
-            warnings.warn(warning.format(self.name, 'loghot',
-                          self.params['loghot']))
+        #warning = ('\n For {0} {1}={2}, which is '
+        #           'larger than acceptable. \n')
+        #if self.params['logm7g'] > -1.0:
+        #    warnings.warn(warning.format(self.path, 'logm7g',
+        #                  self.params['logm7g']))
+        #elif self.params['Teff'] > -1.0:
+        #    warnings.warn(warning.format(self.path, 'Teff',
+        #                  self.params['Teff']))
+        #elif self.params['loghot'] > -1.0:
+        #    warnings.warn(warning.format(self.path, 'loghot',
+        #                  self.params['loghot']))
 
         ## Change to read in from *.bestp
         #self.nwalks = 1024
@@ -167,50 +172,84 @@ class Alf(object):
         self.params_fe = dict(zip(new_labels, abundances))
         self.errors_fe = dict(zip(new_labels, errors))
 
-
-    def plot_model(self, wrange, outpath, fname):
-        fig = plt.figure(figsize=(14,9), facecolor='white')
-        ax1 = plt.subplot2grid((3,2), (0,0), rowspan=2, colspan=2)
-        ax2 = plt.subplot2grid((3,2), (2,0), rowspan=1, colspan=2)
-
+    def plot_model(self, outpath, info, mock=False):
         velz = self.params['velz']
         in_wave = self.indata[:,0]/(1.+velz*1e3/constants.c)
         mod_wave = self.spec[:,0]/(1.+velz*1e3/constants.c)
 
-        model = np.interp(in_wave, mod_wave, self.spec[:,1])
+        # Find overlapping wavelength range
+        min_ = max([in_wave[0], mod_wave[0]])
+        max_ = min([in_wave[-1], mod_wave[-1]])
 
-        i = (in_wave > wrange[0]) & (in_wave < wrange[1])
-        coeffs = np.polynomial.chebyshev.chebfit(in_wave[i],
-                    self.indata[:,1][i], 2)
-        poly = np.polynomial.chebyshev.chebval(in_wave[i],
-                    coeffs)
-        ax1.plot(in_wave[i], self.indata[:,1][i]/poly, 'k-', lw=2,
-                    label='Data')
+        i = ((in_wave >= min_) & (in_wave <= max_))
+        in_wave = in_wave[i]
+        in_spec = self.indata[:,1][i]
+        in_erro = self.indata[:,2][i]
 
-        i = (mod_wave > wrange[0]) & (mod_wave < wrange[1])
-        coeffs = np.polynomial.chebyshev.chebfit(mod_wave[i],
-                    self.spec[:,1][i], 2)
-        poly = np.polynomial.chebyshev.chebval(mod_wave[i],
-                    coeffs)
-        ax1.plot(mod_wave[i], self.spec[:,1][i]/poly, color='#E32017',
-                    lw=2, label='Model')
-        ax1.legend(frameon=False)
+        i = ((mod_wave >= min_) & (mod_wave <= max_))
+        mod_wave = mod_wave[i]
+        mod_spec = self.spec[:,1][i]
 
-        i = (in_wave > wrange[0]) & (in_wave < wrange[1])
-        ax2.plot(in_wave[i], (model[i]-self.indata[:,1][i])/model[i]*1e2,
-                    color='#7156A5', lw=2, alpha=0.7)
-        ax2.fill_between(in_wave[i], -self.indata[:,2][i]/self.indata[:,1][i]*1e2,
-                            self.indata[:,2][i]/self.indata[:,1][i]*1e2,
-                            color='#CCCCCC')
-        ax2.set_ylim(-4.9, 4.9)
+        model = np.interp(in_wave, mod_wave, mod_spec)
 
-        ax1.set_ylabel(r'Flux (arbitrary units)',fontsize=22)
-        ax2.set_ylabel(r'Residual $\rm \%$',fontsize=22)
+        chunk = 1000
+        num = int(max_ - min_)/chunk + 1
 
-        ax2.set_xlabel(r'Wavelength $(\AA)$',fontsize=22, labelpad=10)
-        ax2.set_xlabel(r'Wavelength $(\AA)$',fontsize=22, labelpad=10)
+        if not mock:
+            fstring = (
+                       '{0}/{1}_{2}_ssp{3}_fit{4}_imf{5}_'
+                       'nad{6}_bh{7}_ns{8}_wd{9}_model_compare.pdf'
+                       )
+            fname = fstring.format(outpath,
+                    self.legend.replace(' ', '_'),
+                    info['instrument'], info['ssp_type'],
+                    info['fit_type'], info['imf_type'],
+                    info['nad'], info['bh_remnants'],
+                    info['ns_remnants'], info['wd_remnants'])
+        else:
+            fname = '{0}/{1}_model_compare.pdf'.format(outpath, info['in_sigma'])
+        print fname
+        with PdfPages(fname) as pdf:
+            for i in range(0, num):
+                k = ((mod_wave >= min_+chunk*i) & (mod_wave <= min_+chunk*(i+1)))
+                if not np.any(k) or len(mod_wave[k]) <= 10:
+                    continue
 
-        plt.savefig('{0}/{1}'.format(outpath, fname))
+                j = ((in_wave >= min(mod_wave[k])) & (in_wave <= max(mod_wave[k])))
+
+                fig = plt.figure(figsize=(14,9), facecolor='white')
+                ax1 = plt.subplot2grid((3,2), (0,0), rowspan=2, colspan=2)
+                ax2 = plt.subplot2grid((3,2), (2,0), rowspan=1, colspan=2)
+
+                coeffs = np.polynomial.chebyshev.chebfit(in_wave[j],
+                            in_spec[j], 2)
+                poly = np.polynomial.chebyshev.chebval(in_wave[j],
+                            coeffs)
+                ax1.plot(in_wave[j], in_spec[j]/poly, 'k-', lw=2,
+                            label='Data')
+
+                coeffs = np.polynomial.chebyshev.chebfit(mod_wave[k],
+                            mod_spec[k], 2)
+                poly = np.polynomial.chebyshev.chebval(mod_wave[k],
+                            coeffs)
+                ax1.plot(mod_wave[k], mod_spec[k]/poly, color='#E32017',
+                            lw=2, label='Model')
+                ax1.legend(frameon=False)
+
+                ax2.plot(in_wave[j], (model[j]-in_spec[j])/model[j]*1e2,
+                            color='#7156A5', lw=2, alpha=0.7)
+                ax2.fill_between(in_wave[j],
+                        -in_erro[j]/in_spec[j]*1e2,
+                        in_erro[j]/in_spec[j]*1e2,
+                        color='#CCCCCC')
+                ax2.set_ylim(-4.9, 4.9)
+
+                ax1.set_ylabel(r'Flux (arbitrary units)',fontsize=22)
+                ax2.set_ylabel(r'Residual $\rm \%$',fontsize=22)
+
+                ax2.set_xlabel(r'Wavelength $(\AA)$',fontsize=22, labelpad=10)
+
+                pdf.savefig()
 
     def plot_corner(self, outpath):
         import corner
@@ -231,12 +270,24 @@ class Alf(object):
         figure = corner.corner(self.mcmc[:,use[0]], labels=labels[use[0]])
 
         plt.tight_layout()
-        plt.savefig('{0}/{1}_corner.pdf'.format(outpath, self.name))
+        plt.savefig('{0}/{1}_corner.pdf'.format(outpath, self.legend))
 
 
-    def plot_traces(self, path):
-        outname = ('{0}/{1}_traces.pdf'.format(path, self.name))
+    def plot_traces(self, outpath, info, mock=False):
+        if not mock:
+            fstring = (
+                       '{0}/{1}_{2}_ssp{3}_fit{4}_imf{5}_'
+                       'nad{6}_bh{7}_ns{8}_wd{9}_traces.pdf'
+                       )
+            outname = fstring.format(outpath,
+                    self.legend.replace(' ', '_'),
+                    info['instrument'], info['ssp_type'],
+                    info['fit_type'], info['imf_type'],
+                    info['nad'], info['bh_remnants'],
+                    info['ns_remnants'], info['wd_remnants'])
 
+        else:
+            outname = '{0}/{1}_traces.pdf'.format(outpath, info['in_sigma'])
         plt.cla()
         plt.clf()
 
@@ -245,19 +296,21 @@ class Alf(object):
             self.mcmc = np.loadtxt(fname)
 
         self.nchain = 100
-        self.nwalks = 1020
+        self.nwalks = 510
 
         num = len(self.params)
         data = np.zeros((self.nchain, self.nwalks, num))
         for i in range(0, self.nchain):
             for j in range(0,self.nwalks):
-                data[i,j] = self.mcmc[i*1020+j]
+                data[i,j] = self.mcmc[i*510+j]
+
+
 
         with PdfPages(outname) as pdf:
             for i, (label, trace) in enumerate(zip(self.labels, data.T)):
                 fig = plt.figure(figsize=(8,6), facecolor='white')
-                if i == 0: # Don't care to see the chi^2 value
-                    continue
+                #if i == 0: # Don't care to see the chi^2 value
+                #    continue
                 plt.plot(np.arange(0, self.nchain),
                          data[:,:,i], color='k', alpha=0.1)
                 plt.axhline(self.params[label], color='#3399ff')
@@ -267,7 +320,7 @@ class Alf(object):
                 plt.close()
                 plt.cla()
 
-    def plot_posterior(self, path):
+    def plot_posterior(self, path, info, mock=False):
         plt.cla()
         plt.clf()
 
@@ -275,17 +328,15 @@ class Alf(object):
             fname = '{0}.mcmc'.format(self.path)
             self.mcmc = np.loadtxt(fname)
 
-        fig, axarr = plt.subplots(7, 7, figsize=(40,40),facecolor='white')
+        fig, axarr = plt.subplots(7, 8, figsize=(40,40),facecolor='white')
         axarr = axarr.reshape(axarr.size,1).copy()
         plt.tick_params(axis='both', which='major', labelsize=15)
         plt.tick_params(axis='both', which='minor', labelsize=10)
 
         for i, label in enumerate(self.labels):
-            if (label=='chi2' or label=='ML_k' or
-                label == 'MW_k' or
+            if (label=='ML_k' or label == 'MW_k' or
                 np.isnan(self.params[label])==True):
                 continue
-
             axarr[i-1][0].set_ylabel(label, fontsize=16, labelpad=30)
 
             axarr[i-1][0].hist(self.mcmc[:,i], bins=30,
@@ -293,10 +344,23 @@ class Alf(object):
                               lw=2, alpha=0.9)
             axarr[i-1][0].axvline(self.params[label], color='#E32017',
                                    alpha=0.85)
-            axarr[i-1][0].autoscale(tight=True)
+            #axarr[i-1][0].autoscale(tight=True)
 
         plt.tight_layout()
-        plt.savefig('{0}/{1}_posteriors.pdf'.format(path, self.name))
+        if not mock:
+            fstring = (
+                       '{0}/{1}_{2}_ssp{3}_fit{4}_imf{5}_'
+                       'nad{6}_bh{7}_ns{8}_wd{9}_posterior.pdf'
+                       )
+            fname = fstring.format(path,
+                    self.legend.replace(' ', '_'),
+                    info['instrument'], info['ssp_type'],
+                    info['fit_type'], info['imf_type'],
+                    info['nad'], info['bh_remnants'],
+                    info['ns_remnants'], info['wd_remnants'])
+        else:
+            fname = '{0}/{1}_posterior.pdf'.format(path, info['in_sigma'])
+        plt.savefig(fname)
 
     def write_params(self):
         fname = '{0}_parameter_values.txt'.format(self.path)
